a48c63201f9920523fe1a29f8223e4e377d84830
raeedah <raeedah.w@gmail.com>
2020-09-24 00:01:19 -0400

    updated build script, need to fix navbar

--- src/script.js
+++ src/script.js
@@ -8,25 +8,22 @@ import { spectate as spectateConfig } from '../package.json';
 
 initiatePage();
 
-// Fade in navbar at scroll trigger
+
+/* Fade in navbar at scroll trigger */
 
 const navbar = document.getElementById('navbar');
+enterView({
+  selector: USE_COVER_HED ? '.headline' : '.step-deck',
+  offset: USE_COVER_HED ? 1 : 0.957,
+  enter: () => {
+    navbar.classList.remove('only-logo');
+  },
+  exit: () => {
+    navbar.classList.remove('show-nav-links');
+    navbar.classList.add('only-logo');
+  },
+});
 
-const { USE_NEWS_NAV, USE_EYE_NAV, USE_COVER_HED } = spectateConfig;
-if (USE_NEWS_NAV || USE_EYE_NAV || USE_COVER_HED) {
-  intersectTop({
-    node: document.getElementById('headline'),
-    onEnter: () => {
-      navbar.classList.remove('only-eye-logo');
-      navbar.classList.remove('hide-news-navbar');
-    },
-    onExit: () => {
-      navbar.classList.remove('show-nav-links');
-      navbar.classList.add('only-eye-logo');
-      navbar.classList.add('hide-news-navbar');
-    },
-  });
-}
 
 // Mobile navbar hamburger trigger
 
